a02f7f90aa15ed5c2193b4bf0bbef194ac9df7d7
Lucas <lucasnrgaard@gmail.com>
2025-02-16 11:19:34 +0100

    refactor: update version handling and improve category retrieval logic

--- src/routes/v1_categories.ts
+++ src/routes/v1_categories.ts
@@ -1,6 +1,6 @@
 import type { HonoContext } from "../types";
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { createError } from "../utils";
+import { createError, getAvailableVersions } from "../utils";
 import { ALL_CATEGORIES_ROUTE } from "./v1_categories.openapi";
 
 export const V1_CATEGORIES_ROUTER = new OpenAPIHono<HonoContext>().basePath("/api/v1/categories/:version");
@@ -12,20 +12,28 @@ V1_CATEGORIES_ROUTER.use(async (c, next) => {
     return createError(c, 400, "missing version");
   }
 
-  // const availableVersions = await getAvailableVersions();
+  const availableVersions = await getAvailableVersions();
 
-  // if (!availableVersions.includes(version) && version !== "latest") {
-  //   return createError(c, 400, "invalid version");
-  // }
+  if (version !== "latest" && !availableVersions.some((v) => v.emoji_version === version)) {
+    return createError(c, 404, "version not found");
+  }
 
   await next();
 });
 
 V1_CATEGORIES_ROUTER.openapi(ALL_CATEGORIES_ROUTE, async (c) => {
-  const _version = c.req.param("version");
+  const version = c.req.param("version");
+
+  const res = await fetch(`https://raw.githubusercontent.com/mojisdev/emoji-data/refs/heads/main/data/v${version}/groups.json`);
+
+  if (!res.ok) {
+    return createError(c, 500, "failed to fetch categories");
+  }
+
+  const categories = await res.json();
 
   return c.json(
-    [],
+    categories as [],
     200,
   );
 });
