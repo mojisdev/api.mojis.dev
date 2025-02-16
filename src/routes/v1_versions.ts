51dfad2e92ffdbeef7064a38eb009613139545c2
Lucas <lucasnrgaard@gmail.com>
2025-02-16 09:05:37 +0100

    chore: lint

--- src/routes/v1_versions.ts
+++ src/routes/v1_versions.ts
@@ -1,5 +1,5 @@
 import type { HonoContext } from "../types";
-import { OpenAPIHono, z } from "@hono/zod-openapi";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { EMOJI_LOCK_SCHEMA } from "../schemas";
 import { createError, getCachedVersions } from "../utils";
 import { ALL_EMOJI_VERSIONS_ROUTE, LATEST_EMOJI_VERSIONS_ROUTE } from "./v1_versions.openapi";
