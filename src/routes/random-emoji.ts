f40011d8baf20d02ec9cdbadbfbc6e93df3ed1dd
Lucas <lucasnrgaard@gmail.com>
2025-03-01 06:43:36 +0100

    chore: set caching

--- src/routes/random-emoji.ts
+++ src/routes/random-emoji.ts
@@ -1,9 +1,10 @@
 import type { HonoContext } from "../types";
 import { Hono } from "hono";
+import { cache } from "../utils";
 
 export const RANDOM_EMOJI_ROUTER = new Hono<HonoContext>();
 
 RANDOM_EMOJI_ROUTER.get("/random-emoji.png", async (c) => {
-  // TODO: implement our own random emoji endpoint
+  cache(c, 60 * 60);
   return c.redirect("https://image.luxass.dev/api/image/emoji");
 });
