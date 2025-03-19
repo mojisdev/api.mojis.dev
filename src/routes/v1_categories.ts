592684d21c32a13783d040608fe42270567cde2e
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-19 06:03:01 +0100

    fix: update category fetching to use environment variable and improve error handling

--- src/routes/v1_categories.ts
+++ src/routes/v1_categories.ts
@@ -1,8 +1,9 @@
 import type { HonoContext } from "../types";
 import { OpenAPIHono, z } from "@hono/zod-openapi";
+import { HTTPException } from "hono/http-exception";
 import { versionMiddleware } from "../middlewares/version";
 import { EmojiCategorySchema } from "../schemas";
-import { createError } from "../utils";
+import { cache, createError } from "../utils";
 import { ALL_CATEGORIES_ROUTE, GET_CATEGORY_ROUTE } from "./v1_categories.openapi";
 
 export const V1_CATEGORIES_ROUTER = new OpenAPIHono<HonoContext>().basePath("/api/v1/categories/:version");
@@ -12,10 +13,12 @@ V1_CATEGORIES_ROUTER.use(versionMiddleware);
 V1_CATEGORIES_ROUTER.openapi(ALL_CATEGORIES_ROUTE, async (c) => {
   const version = c.req.param("version");
 
-  const res = await fetch(`https://raw.githubusercontent.com/mojisdev/emoji-data/refs/heads/main/data/v${version}/groups.json`);
+  const res = await c.env.EMOJI_DATA.get(`v${version}/groups.json`);
 
-  if (!res.ok) {
-    return createError(c, 500, "failed to fetch categories");
+  if (res == null) {
+    throw new HTTPException(500, {
+      message: "failed to fetch categories",
+    });
   }
 
   const data = await res.json();
@@ -28,6 +31,8 @@ V1_CATEGORIES_ROUTER.openapi(ALL_CATEGORIES_ROUTE, async (c) => {
 
   const categories = result.data;
 
+  cache(c, 3600, true);
+
   return c.json(
     categories,
     200,
@@ -38,10 +43,12 @@ V1_CATEGORIES_ROUTER.openapi(GET_CATEGORY_ROUTE, async (c) => {
   const version = c.req.param("version");
   const categorySlug = c.req.param("category");
 
-  const res = await fetch(`https://raw.githubusercontent.com/mojisdev/emoji-data/refs/heads/main/data/v${version}/groups.json`);
+  const res = await c.env.EMOJI_DATA.get(`v${version}/groups.json`);
 
-  if (!res.ok) {
-    return createError(c, 500, "failed to fetch categories");
+  if (res == null) {
+    throw new HTTPException(500, {
+      message: "failed to fetch categories",
+    });
   }
 
   const data = await res.json();
@@ -53,6 +60,8 @@ V1_CATEGORIES_ROUTER.openapi(GET_CATEGORY_ROUTE, async (c) => {
   }
 
   const category = result.data.find((c) => c.slug === categorySlug);
+
+  cache(c, 3600, true);
 
   return c.json(
     category,
